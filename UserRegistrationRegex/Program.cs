cf3b6d4c0268dda8a01dc6c1b4e67675786a3d6f
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-14 19:02:13 +0530

    Welcome Message

---
+++ UserRegistrationRegex/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationRegex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration for Validation \n");
+        }
+    }
+}
