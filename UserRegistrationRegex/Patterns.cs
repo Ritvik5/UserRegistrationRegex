bfa0ae76f45dd5292a93044707a8bbd7af7d2565
Ritvik5 <sharma.ritvik56@gmail.com>
2023-03-14 23:16:53 +0530

    Validation for FirstName

---
+++ UserRegistrationRegex/Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegex
+{
+    public class Patterns
+    {
+        public static string Regex_Rule = "^[A-Z][a-z]{2,}$";
+
+        public bool ValidateFirstName(string inputString)
+        {
+            return Regex.IsMatch(inputString, Regex_Rule);
+        }
+    }
+}
--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace UserRegistrationRegex
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration for Validation \n");
+            
+            Console.WriteLine("Enter the First Name : \n");
+            string firstName = Console.ReadLine();
+
+            Patterns patterns = new Patterns();
+            bool isValid = patterns.ValidateFirstName(firstName);
+            if (isValid)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
         }
     }
 }
